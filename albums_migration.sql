9a0378e36680306f6465b3f220e069da62516b03
Hannah Paterka <paterkahannah@gmail.com>
2021-06-17 16:09:18 -0500

    completed exercise

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(50),
+    name VARCHAR(100) NOT NULL,
+    release_date DATE,
+    sales FLOAT,
+    genre VARCHAR(50),
+    PRIMARY KEY(id)
+);

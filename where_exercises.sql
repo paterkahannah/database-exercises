5f692918b90bed1487f9efaad1991fca20ee1265
Hannah Paterka <paterkahannah@gmail.com>
2021-06-21 12:04:37 -0500

    exercise complete

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,45 +1,45 @@
 -- exercise part 1
 -- 2. Find all employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN).
-select * from employees where first_name in ('Irena', 'Vidya', 'Maya');
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 
 -- 3. Find all employees whose last name starts with 'E' — 7,330 rows.
-select * from employees where last_name like 'E%';
+SELECT * FROM employees WHERE last_name LIKE 'E%';
 
 -- 4. Find all employees with a 'q' in their last name — 1,873 rows.
-select * from employees where last_name like '%q%';
+SELECT * FROM employees WHERE last_name LIKE '%q%';
 
 -- exercise part 2
 -- 1. Update your query for 'Irena', 'Vidya', or 'Maya' to use OR instead of IN — 709 rows.
- select * from employees where (first_name = 'Irena'
- or first_name = 'Vidya'
- or first_name = 'Maya')
+SELECT * FROM employees WHERE (first_name = 'Irena'
+OR first_name = 'Vidya'
+OR first_name = 'Maya')
 -- 2. Add a condition to the previous query to find everybody with those names who is also male — 441 rows.
- and gender = 'm';
+AND gender = 'm';
 
 -- 3. Find all employees whose last name starts or ends with 'E' — 30,723 rows.
-select * from employees where last_name like 'e%' or last_name like '%e';
+SELECT * FROM employees WHERE last_name LIKE 'e%' OR last_name LIKE '%e';
 
 -- 4. Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows.
-select * from employees where last_name like 'e%' and last_name like '%e';
+SELECT * FROM employees WHERE last_name LIKE 'e%e';
 
 -- 5. Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
-select * from employees where last_name like '%q%' and last_name not like '%qu%';
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
 
 -- NOTES:
-select * from employees where hire_date = '1985-01-01';
+SELECT * FROM employees WHERE hire_date = '1985-01-01';
 
-select first_name from employees where first_name like '%sus%';
+SELECT first_name FROM employees WHERE first_name LIKE '%sus%';
 
-select emp_no, first_name ,last_name from employees where emp_no between 10026 and 10082;
+SELECT emp_no, first_name ,last_name FROM employees WHERE emp_no BETWEEN 10026 AND 10082;
 
-select emp_no, first_name ,last_name from employees where last_name in ('Shridhar');
+SELECT emp_no, first_name ,last_name FROM employees WHERE last_name IN ('Shridhar');
 
-select emp_no, first_name ,last_name from employees
-where emp_no < 20000 and last_name in ('Herber', 'Baek')
-   or first_name = 'Shridhar';
+SELECT emp_no, first_name ,last_name FROM employees
+WHERE emp_no < 20000 AND last_name IN ('Herber', 'Baek')
+OR first_name = 'Shridhar';
 
-select * from salaries where salary not between 50000 and 100000; # both these work
-select * from salaries where not salary between 50000 and 100000; # both these work
+SELECT * FROM salaries WHERE salary NOT BETWEEN 50000 AND 100000; -- # both these work
+SELECT * FROM salaries WHERE NOT salary BETWEEN 50000 AND 100000; -- # both these work
 
 SELECT birth_date FROM employees
 WHERE NOT birth_date LIKE '195%';

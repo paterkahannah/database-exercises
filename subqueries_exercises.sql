889301a9ef549f1f6d3ae272d51bf404ab5cb7fd
Hannah Paterka <paterkahannah@gmail.com>
2021-06-28 09:40:53 -0500

    exercises and bonus done

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -37,12 +37,11 @@ WHERE dept_no IN (
         )
     );
 
--- -- Find the first and last name of the employee with the highest salary.
--- SELECT first_name, last_name
--- FROM employees
--- WHERE emp_no IN (
---     SELECT emp_no
---     FROM salaries
---     WHERE salary = MAX(salary)
--- )
--- LIMIT 1;
+-- Find the first and last name of the employee with the highest salary.
+SELECT first_name, last_name
+FROM employees
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM salaries
+    WHERE salary = (SELECT MAX(salary) from salaries)
+);
